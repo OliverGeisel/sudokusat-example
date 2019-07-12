09db896c45ec3e92e11a4ffe02b55a771a8f1b0b
Oliver <geisel.oliver@googlemail.com>
2019-07-13 00:16:23 +0200

    Fix issue #6

--- my_solver/oliver/decoder/Decoder.py
+++ my_solver/oliver/decoder/Decoder.py
@@ -6,6 +6,10 @@ from my_solver.oliver.PuzzleInfo import PuzzleInfoOutput, PuzzleInfoEncode
 from my_solver.oliver.encoder.Encoder import convert_var_into_pos
 
 
+class UnsatisfiableException(Exception):
+    pass
+
+
 def fill_output_field(output_field: List[str], variables: List[str], info: PuzzleInfoOutput) -> None:
     line_start = "| "
     width = math.ceil(math.log(info.length, 10))
@@ -28,6 +32,7 @@ def create_sep_line(length) -> str:
     start_separator = 1
     sqrt_of_length = int(math.sqrt(length))
     separator_between_cells = 1
+    # ToDo join()?
     return ("+"
             + "-"
             * (start_separator
@@ -41,25 +46,30 @@ def create_sep_line(length) -> str:
 def decode(encode_info: PuzzleInfoEncode) -> None:
     info = PuzzleInfoOutput(encode_info)
     path = info.input_file_complete_absolute()
-    filled_sudoku = read_source(path, info)
+    try:
+        filled_sudoku = read_source(path, info)
+    except UnsatisfiableException:
+        return
     write_solution_file(info, filled_sudoku)
-    pass
 
 
 def read_source(source_path: str, info: PuzzleInfoOutput) -> List[str]:
     with open(source_path) as solution:
         content = solution.readlines()
-    #  remove 'v' and split string into single strings
 
-    satisfiable = list(filter(lambda x: x[0] == "s", content))[0]
+    satisfiable = [x for x in content if x[0] == "s"][0]
     if "SATISFIABLE".lower() not in satisfiable.lower():
         write_solution_file(info, "NO Solution\n")
+        raise UnsatisfiableException
 
-    # ToDo get all lines that starts with v
-    variables = content[-1].replace("v ", "").split()
-    # get only positive values
-    # variables = [x for x in variables if int(x) > 0]
-    variables = list(filter(lambda x: int(x) > 0, variables))
+    #  remove 'v'
+    var_lines = [x for x in content if x[0] == "v"]
+    var_lines = list(map(lambda x: x.replace("v ", ""), var_lines))
+    variables = list()
+    for current_line in var_lines:
+        # get only positive values
+        current_vars = [x for x in current_line.split() if int(x) > 0]
+        variables.extend(current_vars)
     # get concrete values from variables
     output_field = list()
     fill_output_field(output_field, variables, info)
@@ -69,11 +79,12 @@ def read_source(source_path: str, info: PuzzleInfoOutput) -> List[str]:
 
 
 def add_horizontal_lines(info, output_field):
+    sep_line = create_sep_line(info.length)
     for i in range(info.sqrt_of_length + 1):
-        output_field.insert((info.sqrt_of_length + 1) * i, create_sep_line(info.length))
+        output_field.insert((info.sqrt_of_length + 1) * i, sep_line)
 
 
-def write_solution_file(info, output_field):
+def write_solution_file(info, main_output):
     with open(info.output_file_complete_absolute(), "w") as output:
         output.writelines(info.text)
-        output.writelines(output_field)
+        output.writelines(main_output)
